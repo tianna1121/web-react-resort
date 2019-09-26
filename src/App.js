c6bce6ebee7e5ecd6b1ffea1d4a2c5ec26d1e283
yang zhang <tianna1121@gmail.com>
2019-09-26 20:50:00 +0800

    add error page with Switch

--- src/App.js
+++ src/App.js
@@ -11,9 +11,12 @@ import { Route, Switch } from "react-router-dom";
 function App() {
   return (
     <>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/rooms" component={Rooms} />
-      <Route exact path="/single-room" component={SingleRoom} />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
     </>
   );
 }
