e514aef1f288f12bdebeba1f24d91b2d2a731d60
yang zhang <tianna1121@gmail.com>
2019-09-27 16:39:01 +0800

    add capacity filter to the room page.

--- src/components/RoomFilter.js
+++ src/components/RoomFilter.js
@@ -36,6 +36,15 @@ export default function RoomFilter({ rooms }) {
     );
   });
 
+  let people = getUnique(rooms, "capacity");
+  people = people.map((item, index) => {
+    return (
+      <option key={index} value={item}>
+        {item}
+      </option>
+    );
+  });
+
   return (
     <section className="filter-container">
       <Title title="search rooms" />
@@ -54,6 +63,21 @@ export default function RoomFilter({ rooms }) {
           </select>
         </div>
         {/* end select type */}
+
+        {/* guests */}
+        <div className="form-group">
+          <label htmlFor="capacity">guests</label>
+          <select
+            name="capacity"
+            id="capacity"
+            value={capacity}
+            className="form-control"
+            onChange={handleChange}
+          >
+            {people}
+          </select>
+        </div>
+        {/* end guests */}
       </form>
     </section>
   );
