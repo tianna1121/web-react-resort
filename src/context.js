92f58ab04e28fbb8c6e564e1bc1b5053367209bf
yang zhang <tianna1121@gmail.com>
2019-09-27 16:46:50 +0800

    add price filter of the room page.

--- src/context.js
+++ src/context.js
@@ -94,6 +94,9 @@ class RoomProvider extends Component {
       tempRooms = tempRooms.filter(room => room.capacity >= capacity);
     }
 
+    // * filter by price
+    tempRooms = tempRooms.filter(room => room.price <= price);
+
     this.setState({
       sortedRooms: tempRooms
     });
