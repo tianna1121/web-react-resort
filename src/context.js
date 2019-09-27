7164d197a2c2c6d9dc638b4b1c603c0e693ccd2e
yang zhang <tianna1121@gmail.com>
2019-09-27 16:30:11 +0800

    get rooms according to the type

--- src/context.js
+++ src/context.js
@@ -53,14 +53,39 @@ class RoomProvider extends Component {
   };
 
   handleChange = event => {
-    const type = event.target.type;
+    const target = event.target;
+    const value = event.type === "checkbox" ? target.checked : target.value;
     const name = event.target.name;
-    const value = event.target.value;
-    console.log(type, name, value);
+    // console.log(
+    //   `this is type: ${type}, this is name: ${name}, this is value: ${value}`
+    // );
+    this.setState(
+      {
+        [name]: value
+      },
+      this.filterRooms
+    );
   };
 
   filterRooms = () => {
-    console.log("heelo");
+    let {
+      rooms,
+      type,
+      capacity,
+      price,
+      minSize,
+      maxSize,
+      breakfast,
+      pets
+    } = this.state;
+
+    let tempRooms = [...rooms];
+    if (type !== "all") {
+      tempRooms = tempRooms.filter(room => room.type === type);
+    }
+    this.setState({
+      sortedRooms: tempRooms
+    });
   };
 
   render() {
