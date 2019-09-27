32956e9668141d50ccd201e4327c52850ba6399c
yang zhang <tianna1121@gmail.com>
2019-09-27 15:20:58 +0800

    start roomContainer for rooms page via roomConsumer.

---
+++ src/components/RoomContainer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import RoomFilter from "./RoomFilter";
+import RoomList from "./RoomList";
+import { RoomConsumer } from "../context";
+import Loading from "./Loading";
+
+export default function RoomContainer() {
+  return (
+    <RoomConsumer>
+      {value => {
+        const { loading, sortedRooms, rooms } = value;
+
+        return (
+          <div>
+            Hello from RoomContainer
+            <RoomFilter />
+            <RoomList />
+          </div>
+        );
+      }}
+    </RoomConsumer>
+  );
+}
